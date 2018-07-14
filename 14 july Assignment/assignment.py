dd6a06509e646932fe18deff1e60b19bf595dff8
Bhawana-713 <40892004+Bhawana-713@users.noreply.github.com>
2018-07-14 00:28:50 -0700

    Add files via upload
    It is the assignment of the basic python workshop of first week.

---
+++ 14 july Assignment/assignment.py
@@ -0,0 +1,65 @@
+def number_1():
+
+  x="velocity=u+at"
+  print(x)
+
+  y="acceleration = (v - u) / t"
+
+  print(y)
+
+  v = 25
+  u = 0
+  t =10
+
+  acceleration = (v-u)/t
+  print(acceleration)
+
+
+number_1()
+
+
+
+def number_2():
+
+    x = input("Enter your First Name :")
+    y = input("Enter your Last Name :")
+
+    print(x + y)
+
+
+number_2()
+
+
+def number_3_4():
+
+    x = input("Enter any name :")
+
+    print(x*10)
+
+    print(x,end="**\n")
+
+
+number_3_4()
+
+
+
+def number_5():
+    x=[2,5,9,10]
+    print(min(x))
+    print(max(x))
+    x="Hello World"
+    print(len(x))
+    print(pow(2,4))
+    print(ord("#"))
+    a=[1,2,5]
+    print(sum(a))
+    print(round(55.43))
+    print(round(55.555555,3))
+    p='python'
+    print(dir(p))
+    print(chr(98))
+
+
+number_5()
+
+
